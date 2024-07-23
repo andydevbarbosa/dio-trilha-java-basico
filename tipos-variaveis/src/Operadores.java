ffc326ebe62ae396de18210bf29017f6da8628e6
andydevbarbosa <andbaralmeida@gmail.com>
2024-07-23 00:50:03 -0300

    SmartTv

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,10 @@
+public class Operadores {
+
+    public static void main(String[] args) {
+        String nomeUm = "ANDERSON";
+        String nomeDois = "ANDERSON";
+
+
+        System.out.println(nomeUm == nomeDois);
+    }
+}
